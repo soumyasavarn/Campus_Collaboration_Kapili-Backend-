5d5277c8ced0e78b22046bd1c71e9364e26028c7
Soumya Savarn <soumyasavarn2@gmail.com>
2024-06-29 01:13:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 // const io = socketIo(server);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://fontend-campus-collab-bz2u.vercel.app",
         methods: ["GET", "POST"],
     },
 });
